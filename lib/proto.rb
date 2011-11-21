9bb8588a0bb9eca26d5f768c2b8949c8a835a4b1
Christian Naths <christiannaths@gmail.com>
2011-11-21 13:40:13 -0700

    added template files

--- lib/proto.rb
+++ lib/proto.rb
@@ -1,31 +1,47 @@
-require 'proto/version'
 require 'rubygems'
 require 'commander/import'
 require 'fileutils'
 require 'yaml'
+require File.join(File.dirname(__FILE__), 'proto', 'version')
 
 module Proto
   class Setup
 
+
     # Generate a new Proto project
     def initialize(path)
-      PATHS = {
+      @paths = {
         :main => path,
         :views => File.join(path, 'views'),
         :layouts => File.join(path, 'views', 'layouts'),
         :partials => File.join(path, 'views', 'partials'),
         :stylesheets => File.join(path, 'stylesheets'),
-        :proto_styles => File.join(path, 'stylesheets', 'proto'),
-        :proto_examples => File.join(path, 'stylesheets', 'proto', 'examples'),
+        :styles => File.join(path, 'stylesheets', 'proto'),
+        :examples => File.join(path, 'stylesheets', 'proto', 'examples'),
         :javascripts => File.join(path, 'javascripts'),
         :public => File.join(path, 'public'),
-        :public_javascripts => File.join(path, 'public', 'javascripts')
+        :public_javascripts => File.join(path, 'public', 'javascripts'),
       }
 
-      PATHS.each do |key, value|
+      templates_path = File.join(File.dirname(__FILE__), '../', 'templates')
+      @templates = {
+        :app => File.join(templates_path, 'app.rb'),
+        :views => File.join(templates_path, 'views'),
+        :stylesheets => File.join(templates_path, 'stylesheets'),
+        :javascripts => File.join(templates_path, 'javascripts'),
+        :public => File.join(templates_path, 'public')
+      }
+
+
+      @paths.each do |key, value|
         FileUtils.mkdir_p(value)
       end
-      FileUtils.cd(PATH[:main])
+
+      FileUtils.cp( @templates[:app], File.join(@paths[:main], 'app.rb') )
+      FileUtils.cp_r( @templates[:views], @paths[:main] )
+      FileUtils.cp_r( @templates[:stylesheets], @paths[:main] )
+      FileUtils.cp_r( @templates[:javascripts], @paths[:main] )
+      FileUtils.cp_r( @templates[:public], @paths[:main] )
     end
 
   end
---
+++ templates/app.rb
@@ -0,0 +1,62 @@
+require 'sinatra'
+require 'haml'
+require 'sass'
+require 'coffee-script'
+
+set :sass, :style => :expanded
+set :views, ['views', 'views/layouts', 'views/partials', 'stylesheets', 'javascripts']
+
+
+helpers do
+  def link(name, url="#")
+    "<a href='#{ url }'>#{ name }</a>"
+  end
+
+  def img(url)
+    "<img src='#{ url }' alt='' />"
+  end
+
+  def stylesheet(name)
+    "<link rel='stylesheet' type='text/css' href='/#{ name.to_s }.css' />"
+  end
+
+  def javascript(name)
+    if name == :jquery
+      '<script type="text/javascript" src="/javascripts/jquery.min.js"></script>'
+    elsif name == :html5shim
+      '<script type="text/javascript" src="/javascripts/html5shim.min.js"></script>'
+    else
+      "<script type='text/javascript' src='/#{ name.to_s }.js'></script>"
+    end
+  end
+
+  def find_template(views, name, engine, &block)
+    Array(views).each { |v| super(v, name, engine, &block) }
+  end
+end
+
+
+get '/styles.css' do
+  sass :styles
+end
+
+get '/application.js' do
+  coffee :application
+end
+
+get '/' do
+  haml :index
+end
+
+get '/about' do
+  haml :about
+end
+
+
+
+
+
+
+get '/help/columns' do
+  haml :columns
+end
